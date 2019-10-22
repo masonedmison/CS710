3f6b2960b581636c7c6b43b32ce38c619cda16a4
Mason Edmison <mzbaran@uwm.edu>
2019-10-21 20:25:14 -0500

    tweakz

--- hw2/feature_selection.py
+++ hw2/feature_selection.py
@@ -17,7 +17,7 @@ NUM_OPT_FEATURES = 6
 POP = 35 # Population size.
 num_parents_mating = 2 # Number of parents inside the mating pool.
 num_mutations = 2 # Number of elements to mutate.
-num_generations = 8
+num_generations = 12
 ##################################
 
 attributes = ['animal name','hair', 'feathers', 'eggs', 'milk', 'airborne', 'aquatic', 'predator', 'toothed',
@@ -84,7 +84,8 @@ for i in range(POP):
 ###
 
 # best outputs to record the number of features with best fitness score
-best_outputs = []
+best_outputs = dict()
+best_out_seq = []
 new_population = np.zeros(pop_shape)
 for generation in range(num_generations):
     num_of_matings = 0 # also tells us index of children being addded
@@ -94,24 +95,24 @@ for generation in range(num_generations):
         # Measuring the fitness of each chromosome in the population.
         # fitness, fit_num_features = GA.cal_pop_fitness(old_population, zoo1_X, zoo1_Y, train_indices, test_indices, CLASSIFIER)
         parents = np.asarray([GA.get_parent(old_population, zoo1_X, zoo1_Y, train_indices, test_indices, CLASSIFIER) for i in range(2)])
-        # best_outputs.append(np.max(fitness))
-        # The best result in the current iteration.
-        # print("Best result : ", best_outputs[-1])
-
-        # Selecting the best parents in the population for mating.
-        # parents = GA.select_mating_pool(new_population, fitness, num_parents_mating)
 
         # Generating next generation using crossover.
         offspring_crossover = GA.crossover(parents)
         # Adding some variations to the offspring using mutation.
         offspring_mutation = GA.mutation(offspring_crossover)
 
-        # Creating the new population based on the parents and offspring.
-        # new_population[0:parents.shape[0], :] = parents
-        # new_population[parents.shape[0]:, :] = offspring_mutation
+
         new_population[num_of_matings,:] = offspring_mutation # use num_of_matings as index
         num_of_matings += 1
-    print('at end of generation', new_population.shape)
+
+    # get most fit individual at end of generation
+    fitnesses = GA.cal_pop_fitness(new_population, zoo1_X, zoo1_Y, train_indices, test_indices, CLASSIFIER)
+    max_fit = np.max(fitnesses)
+    max_fit_i = np.where(fitnesses == max_fit)
+    max_fit_i = max_fit_i[0][0]
+    best_outputs[max_fit] = new_population[max_fit_i,:]
+
+    best_out_seq.append(max_fit) # to see progression of iters
 
     old_population = new_population # point old_population to new_population as this is where we will look for parents for next gen
 
@@ -121,23 +122,32 @@ for generation in range(num_generations):
 
 # Getting the best solution after iterating finishing all generations.
 # At first, the fitness is calculated for each solution in the final generation.
-fitness, _ = GA.cal_pop_fitness(new_population, zoo1_X, zoo1_Y, train_indices, test_indices, CLASSIFIER)
-# Then return the index of that solution corresponding to the best fitness.
-best_match_idx = np.where(fitness == np.max(fitness))[0]
-best_match_idx = best_match_idx[0]
+# fitness = GA.cal_pop_fitness(new_population, zoo1_X, zoo1_Y, train_indices, test_indices, CLASSIFIER)
+# # Then return the index of that solution corresponding to the best fitness.
+# best_match_idx = np.where(fitness == np.max(fitness))[0]
+# best_match_idx = best_match_idx[0]
 
-best_solution = new_population[best_match_idx, :]
+print('best outputs', best_outputs)
+
+best_fitnesses = list(best_outputs.keys())
+best_fitness = max(best_fitnesses) # key that points to solution and possibly other things
+print(best_fitness)
+best_solution = best_outputs[best_fitness]
+# best_solution = new_population[best_match_idx, :]
 best_solution_indices = np.where(best_solution == 1)[0]
-best_solution_num_elements = best_solution_indices.shape[0]
-best_solution_fitness = fitness[best_match_idx]
+# best_solution_num_elements = best_solution_indices.shape[0]
+# best_solution_fitness = fitness[best_match_idx]
 
-print("best_match_idx : ", best_match_idx)
+
+print('[SOLUTION]')
 print("best_solution : ", best_solution)
 print("Selected indices : ", best_solution_indices)
-print("Number of selected elements : ", best_solution_num_elements)
-print("Best solution fitness : ", best_solution_fitness)
-#
-# matplotlib.pyplot.plot(best_outputs)
-# matplotlib.pyplot.xlabel("Iteration")
-# matplotlib.pyplot.ylabel("Fitness")
-# matplotlib.pyplot.show()
+# print("Number of selected elements : ", best_solution_num_elements)
+print("Best solution fitness : ", best_fitness)
+
+
+
+matplotlib.pyplot.plot(best_out_seq)
+matplotlib.pyplot.xlabel("Iteration")
+matplotlib.pyplot.ylabel("Fitness")
+matplotlib.pyplot.show()
